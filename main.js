ada95e07eb723a9563765b3cff559e55951d9ad3
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-13 13:55:58 +0500

    masala

---
+++ main.js
@@ -0,0 +1,52 @@
+// 1-masala
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   get getInfo() {
+//     return `${this.name}'ning yoshi ${this.age}`;
+//   }
+//   getInfoMethod() {
+//     return `${this.name}'ning yoshi ${this.age}`;
+//   }
+// }
+// const person = new Person("Avazbek", 34.0);
+// console.log(person.getInfo);
+// console.log(person.getInfoMethod());
+
+
+// 2-masala
+// class Labrador {
+//     constructor(name, age, gender, master) {
+//       this.name = name;
+//       this.age = age;
+//       this.gender = gender;
+//       this.species = "Labrador";
+//       this.legs = 4;
+//       this.size = "Large";
+//       this.master = master;
+//       this.loyal = true;
+//     }
+//   }
+// 3-masala
+// class Person {
+//   constructor(name, type) {
+//     this.name = name;
+//     this.age = type;
+//   }
+//   toString() {
+//     return `The animal is a ${this.type} named ${this._name}.`;
+//   }
+//   get name() {
+//     return this._name;
+//   }
+//   set name(newName) {
+//     this._name = newName;
+//   }
+// }
+
+// const myPerson= new Person('Leo','lion');
+// console.log(myPerson.toString());
+// console.log((myPerson.name));
+
